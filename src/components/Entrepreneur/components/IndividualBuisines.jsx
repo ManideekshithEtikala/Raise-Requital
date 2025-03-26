61f45cbc5bf2623689a1c04689a9a575ef113e48
manideekshith <manideekshithgaming@gmail.com>
2025-03-26 19:07:06 +0530

    noting basic changes

--- src/components/Entrepreneur/components/IndividualBuisines.jsx
+++ src/components/Entrepreneur/components/IndividualBuisines.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-
+import { FaPhoneVolume } from "react-icons/fa6";
 const IndividualBuisines = () => {
   const location = useLocation();
   const { model } = location.state || {}; 
@@ -65,8 +65,32 @@ const IndividualBuisines = () => {
             <h2 className="text-xl font-semibold text-gray-700 mb-2">teamDetails</h2>
             <p className="text-gray-600">{model.teamDetails || "N/A"}</p>
           </div>
+          {/* contact us */}
+          <div className="bg-gray-700 p-1 rounded-lg shadow  flex items-center justify-center hover:bg-gray-800">
+                <button className="text-xl font-semibold text-white mb-2 ">Contact With Us </button>
+          </div>
+          <div className="bg-blue-500 hover:bg-blue-700 p-1 rounded-lg shadow flex items-center justify-center">
+                <button className="text-xl font-semibold text-white mb-2">Invest</button>
+          </div>
         </div>
       </div>
+      {/* Sticky button */}
+      <div className="fixed bottom-4 right-4">
+  <div className="group relative flex items-center">
+    {/* Button */}
+    <button
+      className="bg-blue-500 text-white rounded-full p-4 shadow-lg hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-gray-500"
+      aria-label="Contact With Us"
+    >
+        <FaPhoneVolume size={24} fill="white"/>
+    </button>
+
+    {/* Tooltip */}
+    <div className="absolute right-16 bg-blue-500 text-white text-sm font-medium rounded-md px-4 py-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 whitespace-nowrap border border-blue-800">
+                Let&apos;s Have a Talk
+    </div>
+  </div>
+</div>
     </div>
   ) : (
     <div className="container mx-auto px-4 py-8">
